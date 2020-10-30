dbc74860733949c31b01a815f5ade5d230c016a5
ZmengXu <shijiexu2010@gmail.com>
2020-10-30 22:26:44 +0100

    using dlLibraryTable instead of dlopen

--- solverFoam/solverFoam.C
+++ solverFoam/solverFoam.C
@@ -1,46 +1,29 @@
 #include "combustionModel.H"
-#include <stdio.h>
-#include <stdlib.h>
-#include <dlfcn.h>
+#include "IFstream.H"
+#include "dictionary.H"
+#include "dlLibraryTable.H"
+
 using namespace Foam;
 
-//typedef int (*funcType)(int);
 int main(int argc, char *argv[])
 {
     if(argc<2)
     {
-        printf("usage: %s <modelname> <library>\n", argv[0]);
+        printf("usage: %s <modelname>\n", argv[0]);
         exit(1);
     }
 
-    if(argc==2)
-    {
-        const word modelName = argv[1];
-        // RTS call. 
-        combustionModelBase::New(modelName);
-    }
-    else
-    {
-        char *librarypath = argv[2];
+    dictionary controlDict(IFstream("controlDict")());
+    //- Any loaded dynamic libraries. Make sure to construct before
+    //  reading controlDict.
+    dlLibraryTable libraries(controlDict, "libs");
 
-        void *libhandle = dlopen(librarypath, RTLD_LAZY);
+    // Get the name of the algorithm from the arguments passed to the
+    // application. 
+    const word modelName = argv[1];
 
-        if(libhandle == NULL)
-        {
-            perror("dlopen");
-        }
-
-        // Get the name of the algorithm from the arguments passed to the
-        // application. 
-        const word modelName = argv[1];
-
-        // RTS call. 
-        combustionModelBase::New(modelName);
-
-        //funcType opfunc = dlsym(libhandle, "do_operation");
-
-        dlclose(libhandle);
-    }
+    // RTS call. 
+    combustionModelBase::New(modelName);
 
     Info<< "\nEnd\n" << endl;
 
