4b0dd523cd867e1e4714687b21e8f950dc87149b
ZmengXu <shijiexu2010@gmail.com>
2020-11-01 21:45:05 +0100

    get more output

--- ch02/solverFoam/solverFoam.C
+++ ch02/solverFoam/solverFoam.C
@@ -7,16 +7,22 @@ using namespace Foam;
 
 int main(int argc, char *argv[])
 {
+    Info<< "\nEntering main()\n" << endl;
+
     if(argc<2)
     {
         printf("usage: %s <modelname>\n", argv[0]);
         exit(1);
     }
 
+    Info<< "\nLoading the libraries\n" << endl;
+
     dictionary controlDict(IFstream("controlDict")());
     //- Any loaded dynamic libraries. Make sure to construct before
     //  reading controlDict.
     dlLibraryTable libraries(controlDict, "libs");
+
+    Info<< "\nAfter the libraries are loading\n" << endl;
 
     // Get the name of the algorithm from the arguments passed to the
     // application. 
